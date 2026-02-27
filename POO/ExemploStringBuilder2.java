5c990f5b2a52b2c1b845131714d79899cf1dd1dd
Mateus Saullo Felizardo <mateusaullo13@gmail.com>
2026-02-27 10:47:12 -0300

    .

---
+++ POO/ExemploStringBuilder2.java
@@ -0,0 +1,12 @@
+package POO;
+
+public class ExemploStringBuilder2 {
+    public static void main(String[] args) {
+        String nome = "Mateus";
+
+        String nomemaior = nome.toUpperCase();
+
+        System.out.println(nomemaior);
+    }
+    
+}
