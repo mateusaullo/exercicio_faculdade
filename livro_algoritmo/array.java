34ca658f34e48aea98234bb42974c7ab2e8dd5e1
Mateus Saullo Felizardo <mateusaullo13@gmail.com>
2026-03-31 16:43:38 -0300

    .

--- livro_algoritmo/array.java
+++ livro_algoritmo/array.java
@@ -4,14 +4,22 @@ public class array {
     public static void main(String[] args) {
         String[] carro = { "volvo", "BMW","CHEVROLET","MERCEDES","BRASILIA","CAMARO"};
         int atual = 0;
-        int maisbarato = 0;
+        int atual2 = 0;
+        int[] precos = {13000,70000,5000,50000,2000,40000};
 
         for (int i = 0; i < carro.length; i++) {
             atual++;
-            if (carro[i] == "volvo") {
-                System.out.println(+atual+" " +carro[i]+" no array "+i);
+            for (atual2 = 0; atual2 < 6; atual2++) {
+                if (precos[atual2] < i) {
+                    i = atual2;
+                }
+                
+                if (carro[i] == "volvo") {
+                    System.out.println(+atual + " " + carro[i] + " no array " + i + " " +precos[i]);
+                }
             }
+            
         }
-        
+       
     }
 }
