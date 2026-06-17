597418c1ad24aecf67dc2f31907724aa9eb961ed
Mateus Saullo Felizardo <mateusaullo13@gmail.com>
2026-06-17 07:11:26 -0300

    .
    .

--- algoritmos_em_java_busca_ordenacao_analise/classe.java
+++ algoritmos_em_java_busca_ordenacao_analise/classe.java
@@ -3,11 +3,11 @@ package algoritmos_em_java_busca_ordenacao_analise;
 public class classe {
 public static void main(String[] args) {
     Produto produtos[] = new Produto[5];
-    produtos[0] = new Produto( "Lamborghini", 1000000);
-    produtos[1] = new Produto( "Jipe", 46000);
-    produtos[2] = new Produto( "Brasilia", 126000);
-    produtos[3] = new Produto( "Smart", 46000);
-    produtos[4] = new Produto("Fusca", 17000);
+    produtos[0] = new Produto( "Lamborghini", 1000000,"sim");
+    produtos[1] = new Produto( "Jipe", 46000,"nao");
+    produtos[2] = new Produto( "Brasilia", 126000,"nao");
+    produtos[3] = new Produto( "Smart", 46000,"sim");
+    produtos[4] = new Produto("Fusca", 17000,"nao");
     
     int maisbarato = 0;
 
@@ -15,6 +15,10 @@ public static void main(String[] args) {
         if (produtos[atual].getPreco() < produtos[maisbarato].getPreco()) {
             maisbarato = atual;
         }
+        if (produtos[atual].getUsado().equals("sim")) {
+            System.out.println(produtos[atual].getNome() + " é usado");
+        }
+
         produtos[atual].getNome();
         produtos[atual].setPreco(1);
     }
@@ -26,12 +30,14 @@ public static void main(String[] args) {
 
      private String nome;
      private double preco;
+     private String usado;
 
      
 
-     public Produto(String nome, double preco) {
+     public Produto(String nome, double preco, String usado) {
          this.nome = nome;
          this.preco = preco;
+         this.usado = usado;
      }
      
      public double getPreco() {
@@ -39,9 +45,14 @@ public static void main(String[] args) {
      }
 
      public String getNome() {
+
          return nome;
      }
      public void setPreco(double preco) {
+
          this.preco = preco;
+     }
+     public String getUsado() {
+         return usado;
      }
 }
