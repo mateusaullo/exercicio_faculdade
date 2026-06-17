33b44fc24d14407c43e272167071e1557ccad11b
Mateus Saullo Felizardo <mateusaullo13@gmail.com>
2026-06-17 12:26:18 -0300

    refatoracao metodo busca
    refatoracao metodo buscamenor

--- algoritmos_em_java_busca_ordenacao_analise/classe.java
+++ algoritmos_em_java_busca_ordenacao_analise/classe.java
@@ -1,27 +1,33 @@
 package algoritmos_em_java_busca_ordenacao_analise;
 
 public class classe {
-public static void main(String[] args) {
-    Produto produtos[] = new Produto[5];
-    produtos[0] = new Produto( "Lamborghini", 1000000,"sim");
-    produtos[1] = new Produto( "Jipe", 46000,"nao");
-    produtos[2] = new Produto( "Brasilia", 126000,"nao");
-    produtos[3] = new Produto( "Smart", 46000,"sim");
-    produtos[4] = new Produto("Fusca", 17000,"nao");
-    
-    int maisbarato = 0;
+    private static int buscamenor(Produto[] produtos){
+        int maisbarato = 0;
+        int termino = produtos.length-1;
+        for (int atual = 0; atual <= termino; atual++) {
+            if (produtos[atual].getPreco() < produtos[maisbarato].getPreco()) {
+                maisbarato = atual;
+            }
+            if (produtos[atual].getUsado().equals("sim")) {
+                System.out.println(produtos[atual].getNome() + " é usado");
+            }
 
-    for (int atual = 0; atual <= 4; atual++) {
-        if (produtos[atual].getPreco() < produtos[maisbarato].getPreco()) {
-            maisbarato = atual;
-        }
-        if (produtos[atual].getUsado().equals("sim")) {
-            System.out.println(produtos[atual].getNome() + " é usado");
-        }
 
-        produtos[atual].getNome();
-        produtos[atual].setPreco(1);
+        }
+        return maisbarato;
     }
+public static void main(String[] args) {
+    Produto produtos[] = {
+            new Produto ( "Lamborghini", 1000000,"sim"),
+            new Produto( "Jipe", 46000,"nao"),
+            new Produto( "Smart", 46000,"sim"),
+            new Produto("Fusca", 17000,"nao"),
+    };
+
+    
+    int maisbarato = buscamenor(produtos);
+
+
     System.out.println(maisbarato);
     System.out.println("O carro mais barato custa "+produtos[maisbarato].getPreco()+ " "+produtos[maisbarato].getNome());
 }
@@ -41,6 +47,7 @@ public static void main(String[] args) {
      }
      
      public double getPreco() {
+
          return preco;
      }
 
@@ -53,6 +60,10 @@ public static void main(String[] args) {
          this.preco = preco;
      }
      public String getUsado() {
+
          return usado;
      }
-}
+
+
+
+ }
